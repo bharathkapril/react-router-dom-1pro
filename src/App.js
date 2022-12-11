39a8694094788b2246175c38f5dae44ff4590a41
bharathkapril <bharathapril25@gmail.com>
2022-12-11 21:50:06 +0530

    unknown changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import User from "./components/User";
-import Home from "./components/Home";
-
+import { lazy, useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Emoji from "./components/Emoji";
+import axios from "axios";
+import User from "./components/User";
+import Home from "./components/Home";
+// const Home = lazy(() =>
+//   import("./components/Home").then((module) => {
+//     return { default: module.Home };
+//   })
+// );
 
 const URL = "https://jsonplaceholder.typicode.com/users";
 
