0ac8fce5a339997c1ae96aed00c6bce95c08b96f
bharathkapril <bharathapril25@gmail.com>
2022-12-11 21:29:40 +0530

    Added search box in Emoji.js

--- src/components/Emoji.js
+++ src/components/Emoji.js
@@ -1,18 +1,36 @@
+import { useState } from "react";
 import { emoji } from "../emojiDB";
 
-console.log(emoji.map((emo) => emo.emoji));
+// console.log(emoji.map((emo) => emo.emoji));
 
 const Emoji = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div>
       <h1>Emoji</h1>
-      {emoji.map((emo) => {
-        return (
-          <h3>
-            {emo.emoji} -- {emo.description}
-          </h3>
-        );
-      })}
+      <input
+        type="text"
+        placeholder="search..."
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {emoji
+        .filter((val) => {
+          if (val === "") return val;
+          else if (
+            val.description.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+            return val;
+        })
+        .map((emo, key) => {
+          return (
+            <h3 key={key}>
+              {emo.emoji} -- {emo.description} --{emo.aliases}
+              {/* {emo.aliases.map((alias) => (
+              <div>{alias}</div>
+            ))} */}
+            </h3>
+          );
+        })}
       ;
     </div>
   );
